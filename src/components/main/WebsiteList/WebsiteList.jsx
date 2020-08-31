c83c5e88cb6f827e7c651a8328040ac6f24d4c8d
Leopold <leopold.pinkernell@tobit.com>
2020-08-31 15:48:31 +0200

    :sparkles: Add Site List with Livedata

--- src/components/main/WebsiteList/WebsiteList.jsx
+++ src/components/main/WebsiteList/WebsiteList.jsx
@@ -1,35 +1,30 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import WebsiteListItem from './WebsiteListItem/WebsiteListItem';
 import './WebsiteList.scss';
 
-const fetchSitesData = async (skip = 0, take = 20) => {
-    try {
-        const response = await fetch(`https://chayns1.tobit.com/TappApi/Site/SlitteApp?SearchString=Ahaus&Skip=${skip}&Take=${take}`);
-        const json = await response.json();
-        console.log('parsed json', json);
-        return json;
-    } catch (ex) {
-        console.log('parsing failed', ex);
-        return null;
-    }
-};
-
 function WebsiteList() {
+    const [list, setList] = useState([]);
 
-    fetchSitesData(0, 20);
+    const fetchSitesData = async (skip = 0, take = 20) => {
+        try {
+            const response = await fetch(`https://chayns1.tobit.com/TappApi/Site/SlitteApp?SearchString=Ahaus&Skip=${skip}&Take=${take}`);
+            const json = await response.json();
+            console.log(json.Data);
+            setList(json.Data);
+        } catch (ex) {
+            console.log('parsing failed', ex);
+            setList([]);
+        }
+    };
+    useEffect(() => {
+        fetchSitesData();
+    }, []);
+
+    // const websiteListItems = list.map(() => <WebsiteListItem/>);
 
     return (
         <div className="websiteList">
-            <WebsiteListItem/>
-            <WebsiteListItem/>
-            <WebsiteListItem/>
-            <WebsiteListItem/>
-            <WebsiteListItem/>
-            <WebsiteListItem/>
-            <WebsiteListItem/>
-            <WebsiteListItem/>
-            <WebsiteListItem/>
-            <WebsiteListItem/>
+            {list.map((e) => <WebsiteListItem key={e.siteId} name={e.appstoreName} linkId={e.siteId} iconId={e.locationId}/>)}
         </div>
     );
 }
--- src/components/main/WebsiteList/WebsiteListItem/WebsiteListItem.jsx
+++ src/components/main/WebsiteList/WebsiteListItem/WebsiteListItem.jsx
@@ -1,14 +1,33 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+
 
 import './WebsiteListItem.scss';
 
-function WebsiteListItem() {
+function WebsiteListItem({ name, iconId, linkId }) {
+    const [icon, setIcon] = useState(152342);
+
+    const fetchIcon = async () => {
+        try {
+            await fetch(`https://sub60.tobit.com/l/${iconId}?size=65`);
+            setIcon(iconId);
+        } catch {
+            console.log("can't find a website Icon");
+        }
+    };
+    const openUrl = () => {
+        chayns.openUrlInBrowser(`https://chayns.net/${linkId}`);
+    };
+
+    useEffect(() => {
+        fetchIcon();
+    }, []);
+
     return (
         <div className="websiteIconContainer">
-            <a className="websiteIconLink">
-                <img className="websiteIcon" src="" alt=""/>
+            <a className="websiteIconLink" onClick={openUrl}>
+                <img className="websiteIcon" src={`https://sub60.tobit.com/l/${icon}?size=65`} alt=""/>
             </a>
-            <div className="websiteName">Websitename</div>
+            <div className="websiteName">{name}</div>
         </div>
     );
 }
