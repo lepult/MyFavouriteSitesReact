67deabacee30812b4b329aba82caa372911381f7
Leopold <leopold.pinkernell@tobit.com>
2020-08-31 16:58:22 +0200

    :sparkles: Add Extend Button and Load Cursor

--- src/components/main/WebsiteList/WebsiteList.jsx
+++ src/components/main/WebsiteList/WebsiteList.jsx
@@ -1,31 +1,38 @@
 import React, { useEffect, useState } from 'react';
 
-import { Button, ChooseButton } from 'chayns-components/lib';
+import { Button, SmallWaitCursor } from 'chayns-components/lib';
 
 import WebsiteListItem from './WebsiteListItem/WebsiteListItem';
 import './WebsiteList.scss';
 
 function WebsiteList() {
     const [list, setList] = useState([]);
+    const [isListExtendable, setIsListExtendable] = useState(true);
+    const [isListLoading, setIsListLoading] = useState(false);
 
     const fetchSitesData = async (skip = 0, take = 21) => {
+        setIsListLoading(true);
+
         try {
             const response = await fetch(`https://chayns1.tobit.com/TappApi/Site/SlitteApp?SearchString=Ahaus&Skip=${skip}&Take=${take}`);
             const json = await response.json();
-            console.log(json.Data);
 
-            if (!json.Data[20]) {
-                document.querySelector('.extendButton').classList.add('hidden');
+            if (json.Data.length < 21) {
+                setIsListExtendable(false);
+            } else {
+                // removes the 21 item
+                json.Data.length = 20;
             }
 
-            json.Data.length = 20;
-            setList(json.Data);
-
+            setList((prevList) => prevList.concat(json.Data));
         } catch (ex) {
             console.log('parsing failed', ex);
             setList([]);
         }
+
+        setIsListLoading(false);
     };
+
     useEffect(() => {
         fetchSitesData();
     }, []);
@@ -33,9 +40,35 @@ function WebsiteList() {
     // const websiteListItems = list.map(() => <WebsiteListItem/>);
 
     return (
-        <div className="websiteList">
-            {list.map((e) => <WebsiteListItem key={e.siteId} name={e.appstoreName} linkId={e.siteId} iconId={e.locationId}/>)}
-            <Button className="extendButton" type="button"> Mehr </Button>
+        <div className="websiteContainer">
+
+            <div className="websiteList">
+                {list.map((e) => (
+                    <WebsiteListItem
+                        key={e.siteId}
+                        name={e.appstoreName}
+                        linkId={e.siteId}
+                        iconId={e.locationId}
+                    />
+                ))}
+                {isListExtendable && !isListLoading && (
+                    <Button
+                        className="extendButton"
+                        onClick={() => fetchSitesData(list.length)}
+                    >
+                        Mehr
+                    </Button>
+                )}
+            </div>
+
+            {isListLoading && (
+                <SmallWaitCursor
+                    className="waitCursor"
+                    show
+                    inlines
+                />
+            )}
+
         </div>
     );
 }
